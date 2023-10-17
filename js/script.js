4dc099b9badf3ac80e2e57101b2492ef65fe3418
Sergei <yuriev.s.v.dev.it@gmail.com>
2023-10-17 12:50:08 +0300

    add class

--- js/script.js
+++ js/script.js
@@ -149,6 +149,79 @@ window.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener("scroll", showModalByScroll);
 
+    // Class
+
+    class AddCard {
+        constructor(src, alt, title, descr, price, parentselector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+			this.classes = classes;
+            this.parent = document.querySelector(parentselector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price *= this.transfer;
+        }
+
+        render() {
+            const element = document.createElement("div");
+			if (this.classes.length === 0) {
+				this.element = 'menu__item'
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className =>  element.classList.add(className));
+			}
+			
+           
+            element.innerHTML = `	
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+			`;
+            this.parent.append(element);
+        }
+    }
+
+    new AddCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        9,
+        ".menu .container",
+		'menu__item'
+    ).render();
+
+	new AddCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        11,
+        ".menu .container",
+		'menu__item'
+    ).render();
+
+	new AddCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        15,
+        ".menu .container",
+		'menu__item'
+    ).render();
+
     (() => {
         //  Tabes
         hideTabContent();
